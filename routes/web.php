7220ee3f23b6585e95c109076b6e263fa0b8986c
szt2mulben <mulati.bence@gmail.com>
2025-11-11 20:39:13 +0100

    Uzenetek es Admin oldal

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,9 @@
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use Laravel\Fortify\Features;
+use App\Http\Controllers\MessageController;
+use App\Http\Controllers\AdminController;
+
 
 Route::get('/', function () {
     return Inertia::render('welcome', [
@@ -15,5 +18,26 @@ Route::middleware(['auth', 'verified'])->group(function () {
         return Inertia::render('dashboard');
     })->name('dashboard');
 });
+
+
+//Nem bejelentkezett látogatók
+Route::get('/', function () {
+    return Inertia::render('welcome');
+})->name('home');
+
+
+
+// Üzenetek (regisztrált felhasználóknak)
+Route::middleware(['auth'])->group(function () {
+    Route::get('/uzenetek', [MessageController::class, 'index'])->name('messages.index');
+});
+
+
+
+// Admin
+Route::middleware(['auth', 'role:admin'])->group(function () {
+    Route::get('/admin', [AdminController::class, 'index'])->name('admin.index');
+});
+
 
 require __DIR__.'/settings.php';
